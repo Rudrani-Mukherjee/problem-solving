0955b53c20c3580194fcb51d48c35020ebfd300a
Rudrani Mukherjee <rudranimukherjee063@gmail.com>
2026-05-07 12:17:49 +0530

    Time: 638 ms (48.66%), Space: 26.6 MB (75.85%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 32: 
+            self.insertion_sort(nums)
+        else:
+            nums[:] = self.merge_sort(nums)
+        return nums
+    def insertion_sort(self, nums):
+        for i in range(1, len(nums)):
+            key = nums[i]
+            j = i - 1
+            while j >= 0 and nums[j] > key:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = key
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr) // 2
+        left = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+        return self.merge(left, right)
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+        
